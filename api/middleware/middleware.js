5cd40626f06743d68f4b2eaac5404a209d29c668
Devon Osei <oseidevon@gmail.com>
2021-07-08 00:11:00 -0400

    sad

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -28,11 +28,6 @@ async function validateUserId(req, res, next){
   }
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
-  console.log('validateUserId middleware')
-  next()
-}
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
@@ -49,13 +44,13 @@ function validateUser(req, res, next) {
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
-  const { name } = req.body
-  if (!name || !name.trim()) {
+  const { text } = req.body
+  if (!text || !text.trim()) {
     res.status(400).json({
-      message: 'missing required name field',
+      message: 'missing required text field',
     })
   } else {
-    req.text = name.text
+    req.text = text.trim
     next()
   }
   next()
