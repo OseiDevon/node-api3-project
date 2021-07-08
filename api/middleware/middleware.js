162255a937a7196e8ffc7f101736441cceb2e6fb
Devon Osei <oseidevon@gmail.com>
2021-07-07 22:37:44 -0400

    Suwoop

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,71 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+const timestamp = new Data().toLocalString()
+const method = req.method
+const url = req.originalUrl
+
+console.log(`[${timestamp}] ${method} to ${url}`)
+next()
+}
+
+async function validateUserId(req, res, next){
+  try{
+    const user = await User.getById(req.params.id)
+    if (!user) {
+      res.status(404).json({
+        message: 'no such user',
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'problem finding user',
+    })
+  }
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validateUserId middleware')
+  next()
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  const { name } = req.body
+  if (!name || !name.trim()) {
+    res.status(400).json({
+      message: 'missing required name field',
+    })
+  } else {
+    req.name = name.trim
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  const { name } = req.body
+  if (!name || !name.trim()) {
+    res.status(400).json({
+      message: 'missing required name field',
+    })
+  } else {
+    req.text = name.text
+    next()
+  }
+  next()
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
